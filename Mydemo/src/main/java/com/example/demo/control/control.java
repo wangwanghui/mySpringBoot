8c41b60610b5a3f866c5d3799d0a9219bc5799c2
wangwanghui <53170658+wangwanghui@users.noreply.github.com>
2022-11-23 16:06:37 +0800

    Update control.java

--- Mydemo/src/main/java/com/example/demo/control/control.java
+++ Mydemo/src/main/java/com/example/demo/control/control.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/test")
-public class controller {
+public class control {
 
     @GetMapping("/a")
     public Object test() {
